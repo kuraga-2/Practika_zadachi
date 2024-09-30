b8837b5cc2cd972bdddd1baf4d71fdd59e22c000
kuraga-2 <denismanarskij@tutamail.com>
2024-09-30 17:17:49 +0300

    z_5 uprostil

--- z_5.py
+++ z_5.py
@@ -1,10 +1,6 @@
 input_data = open('input.txt', 'r')
 data = input_data.read()
-a1 = int(data)
-A2 = 9
-a3 = A2 - a1
-c=str(a1) + str(A2) + str(a3)
 output_data = open('output.txt', 'w')
-output_data.write(c)
+output_data.write(str(int(data)) + str(9) + str(9 - int(data)))
 input_data.close()
 output_data.close()
